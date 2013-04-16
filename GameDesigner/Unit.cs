c150eeec7a21f99f4e35ee9de71f0fe5ff265599
psacharuk <suchygd@gmail.com>
2013-04-16 23:43:37 +0200

    create behavior of unit class

--- GameDesigner/Unit.cs
+++ GameDesigner/Unit.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace GameDesigner
 {
     public class Unit
     {
+        private string type;
+        private List<Object> properties;
+
         public Unit()
         {
-
+            properties = new List<Object>();
         }
+
+        public string Type { get; set; }
+        public List<Object> Properties { get; set; }
     }
 }
