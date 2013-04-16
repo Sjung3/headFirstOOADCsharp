8bc88af661aee9feb06afe0c43aecf1adbbe8c9e
psacharx <piotrx.sacharuk@intel.com>
2013-04-16 15:32:39 +0200

    GameDesigner

---
+++ GameDesigner/Board.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameDesigner
+{
+    public class Board
+    {
+        private int height, width;
+        private ArrayList tiles;
+
+        public Board(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            tiles = new ArrayList(width);
+            for (int i = 0; i < width; i++)
+            {
+                tiles.Add(new ArrayList(height));
+                for (int j = 0; j < height; j++)
+                {
+                    ((ArrayList)tiles[i]).Add(new Tile());
+                }
+            }
+        }
+
+        public Tile GetTile(int x, int y)
+        {
+            return (Tile)((ArrayList)tiles[x - 1])[y - 1];
+        }
+
+        public void AddUnit(Unit unit, int x, int y)
+        {
+            Tile tile = GetTile(x, y);
+            tile.AddUnit(unit);
+        }
+
+        public void RemoveUnit(Unit unit, int x, int y)
+        {
+            Tile tile = GetTile(x, y);
+            tile.RemoveUnit(unit);
+        }
+
+        public void RemoveUnits(int x, int y)
+        {
+            Tile tile = GetTile(x, y);
+            tile.RemoveUnits();
+        }
+
+        public List<Unit> GetUnits(int x, int y)
+        {
+            return GetTile(x, y).Units;
+        }
+    }
+}
---
+++ GameDesigner/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameDesigner
+{
+    public class Tile
+    {
+        private List<Unit> units;
+
+        public Tile()
+        {
+            units = new List<Unit>();
+        }
+        public void AddUnit(Unit unit)
+        {
+            units.Add(unit);
+        }
+
+        public void RemoveUnit(Unit unit)
+        {
+            units.Remove(unit);
+        }
+
+        public void RemoveUnits()
+        {}
+
+        public List<Unit> Units { get { return units; } }
+
+    }
+}
