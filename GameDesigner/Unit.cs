b9bea4544ce1924807b3e45384a468a00ac940fd
psacharuk <suchygd@gmail.com>
2013-04-18 01:26:41 +0200

    corrected files for compiling code

--- GameDesigner/Unit.cs
+++ GameDesigner/Unit.cs
@@ -5,20 +5,24 @@ namespace GameDesigner
 {
     public class Unit
     {
+        private int id;
         private Dictionary<string, Object> properties;
         private List<Weapon> weapons;
 
         public Unit(int id)
         {
-            id = Id;
+            this.id = id;
             properties = new Dictionary<string, Object>();
             weapons = new List<Weapon>();
         }
 
         public string Type { get; set; }
-        public int Id { get; }
+
+        public int Id { get { return id; } }
+
         public string Name { get; set; }
-        public List<Weapon> Weapons { get {return weapons; } }
+
+        public List<Weapon> Weapons { get { return weapons; } }
 
         public void SetProperty(string property, Object value)
         {
