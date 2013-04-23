2f734436d54d6dc714231e101de0fa1fa74378d0
psacharuk <suchygd@gmail.com>
2013-04-23 19:46:54 +0200

    finished iteration 2

---
+++ ObjectvilleSubwayMap/SubwayTester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjectvilleSubwayMap
+{
+    class SubwayTester
+    {
+        public static void Main2(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: SubwayTester [startStation] [endStation]");
+                Environment.Exit(-1);
+            }
+
+            try
+            {
+                SubwayLoader loader = new SubwayLoader();
+                Subway objectville = loader.LoadFromFile("ObjectvilleSubway.txt");
+
+                if (!objectville.HasStation(args[0]))
+                {
+                    Console.WriteLine(args[0] + " is not a station in Objectville.");
+                    Environment.Exit(-1);
+                }
+                else if (!objectville.HasStation(args[1]))
+                {
+                    Console.WriteLine(args[1] + " is not a station in Objectville.");
+                    Environment.Exit(-1);
+                }
+
+                List<Connection> route = objectville.GetDirections(args[0], args[1]);
+                SubwayPrinter printer = new SubwayPrinter();
+                printer.PrintDirections(route);
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
